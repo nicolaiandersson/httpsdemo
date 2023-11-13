ad1e16cccaf8116fc87555e191d1873c4414663b
nicolaiandersson <nico602d@stud.kea.dk>
2023-11-13 10:35:31 +0100

    :-)

---
+++ src/main/java/dk/kea/httpsdemo/HttpsdemoApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.httpsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpsdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/httpsdemo/HttpsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.httpsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpsdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
